ac105dcf5ec0d681851a2072b6f22fa78abde6be
MeGallin <me@garyallin.uk>
2022-12-29 11:50:41 +0000

    Delete memory working

--- routes/MemoriesRoute.js
+++ routes/MemoriesRoute.js
@@ -5,6 +5,7 @@ const {
   createMemory,
   editMemory,
   deleteMemory,
+  deleteMemoryTag,
 } = require('../controllers/MemoriesController');
 const { protect } = require('../middleWare/authMiddleWare');
 
@@ -12,5 +13,6 @@ router.route('/memories').get(protect, memories);
 router.route('/create-memory').post(protect, createMemory);
 router.route('/edit-memory/:id').put(protect, editMemory);
 router.route('/delete-memory/:id').delete(protect, deleteMemory);
+router.route('/delete-memory-tag/:id').delete(protect, deleteMemoryTag);
 
 module.exports = router;
