adbdf47b7f0e937c64b971fb3c8bb5f1acfec8ce
MeGallin <me@garyallin.uk>
2023-01-02 18:24:01 +0000

    page hits updated route

--- routes/PageHitsRoute.js
+++ routes/PageHitsRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { pageHits } = require('../controllers/PageHitsController');
 
-router.route('/').get(pageHits);
+router.route('/page-hits').get(pageHits);
 
 module.exports = router;
