d1c8bb7d8e9fa20f1cbf8590bae11e4b4db9d83e
MeGallin <me@garyallin.uk>
2023-01-04 12:14:31 +0000

    Upload memory images

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use('/api/', require('./routes/UserRoutes'));
 app.use('/api/', require('./routes/MemoriesRoute'));
 app.use('/api/', require('./routes/ContactFormRoute'));
 app.use('/api/', require('./routes/ConfirmationLinkRoute'));
+app.use('/api/', require('./routes/MemoryUploadImageRoutes'));
+
 const PORT = process.env.PORT || 5000;
 
 // Connect DB
